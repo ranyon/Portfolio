1fcd27e4c2e8ebd25fd606e8400b275b58538818
ranyon <kwakumensahj@gmail.com>
2022-08-17 14:35:31 +0000

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+export const NavBar = () =>{
+
+    
+    return(
+        <Navbar  expand="lg" className={scrolled ? "scrolled": ""}>
+        <Container>
+          <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#skills">Skills</Nav.Link>
+              <Nav.Link href="#project">Project</Nav.Link>
+            </Nav>
+    
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+}
