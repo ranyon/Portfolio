d66aa79a5b53dd5904bfa70221cba4f3394961fc
ranyon <kwakumensahj@gmail.com>
2022-08-19 12:35:11 +0000

    added id

---
+++ src/components/pages/HomePage/Home.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import './Home.css'
+
+const Home =() => {
+  return (
+    <div>
+    <div className="HomeContainer" id='home'>
+      <h3>Home</h3>
+      <p className="Home-paragraph">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+      </p>
+    </div>
+  </div>
+  )
+}
+
+export default Home
