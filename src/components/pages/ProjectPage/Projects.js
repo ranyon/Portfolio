875126e9a98a9a011c0965e952892e06dd325c99
ranyon <kwakumensahj@gmail.com>
2022-08-19 12:36:19 +0000

    added id

---
+++ src/components/pages/ProjectPage/Projects.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Projects.css'
+
+const Projects =() => {
+  return (
+    <div>
+    <div className="ProjectsContainer" id='projects'>
+      <h3>Projects</h3>
+      <p className="Projects-paragraph">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        inventore harum dignissimos error perspiciatis commodi facilis sed
+        architecto cumque quasi.
+      </p>
+    </div>
+  </div>
+  )
+}
+
+export default Projects;
