e969dc1e69c8ed4c47ec9dbd63f044f69ba20f85
ranyon <kwakumensahj@gmail.com>
2022-08-19 12:34:50 +0000

    re imported components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 import { NavBar } from './components/Navbar';
 import Home from './components/pages/HomePage/Home';
 import Skills from './components/pages/SkillsPage/Skills';
-import Projects from './components/pages/ProjectPage/Projects';
+import Projects from './components/pages/ProjectPage/Projects'
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="">
       <NavBar/>
       <Home/>
       <Skills/>
       <Projects/>
-
+      
+      
     </div>
-    </BrowserRouter>
   );
 }
 
