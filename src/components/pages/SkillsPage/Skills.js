311b79cc2b2e74f0483e0039dbb9cc18edfa8f52
ranyon <kwakumensahj@gmail.com>
2022-08-19 12:36:11 +0000

    added id

---
+++ src/components/pages/SkillsPage/Skills.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Skills.css'
+
+const Skills =() => {
+  return (
+    <div>
+      <div className="SkillContainer" id='skils'>
+        <h1>Skills</h1>
+        <p className="skill-paragraph">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+          inventore harum dignissimos error perspiciatis commodi facilis sed
+          architecto cumque quasi.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+          inventore harum dignissimos error perspiciatis commodi facilis sed
+          architecto cumque quasi.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+          inventore harum dignissimos error perspiciatis commodi facilis sed
+          architecto cumque quasi.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+          inventore harum dignissimos error perspiciatis commodi facilis sed
+          architecto cumque quasi.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+          inventore harum dignissimos error perspiciatis commodi facilis sed
+          architecto cumque quasi.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
