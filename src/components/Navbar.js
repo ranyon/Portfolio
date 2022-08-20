e25f436f98d9f4b96ad538aa48698352d7cd9d5b
ranyon <kwakumensahj@gmail.com>
2022-08-20 13:07:58 +0000

    added nav link for scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Navbar, Nav } from 'react-bootstrap';
-import {Link} from 'react-scroll';
+import { useRef } from 'react';
 
 
 
 
 
-export const NavBar = () =>{
-
-    
+export const NavBar = ({forwardedRef}) =>{
+  const home = useRef(null);
+  const skills = useRef(null);
+  const projects = useRef(null);
+ 
+  const scrollToSection = (elementRef) =>{
+    window.scrollTo({
+      top:elementRef.current.offsetTop,
+      behavior:'smooth'
+    })
+  }
     return(
         <Navbar  expand="lg">
         <Container>
@@ -17,9 +25,9 @@ export const NavBar = () =>{
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className='ml-auto'>
-              <Nav.Link href="home">Home</Nav.Link>
-              <Nav.Link href="skills">Skills</Nav.Link>
-              <Nav.Link href="project">Projectt</Nav.Link>
+              <Nav.Link href='#home' >Home</Nav.Link>
+              <Nav.Link  href='#skills'>Skills</Nav.Link>
+              <Nav.Link  href='#projects'>Projectt</Nav.Link>
             </Nav>
     
           </Navbar.Collapse>
