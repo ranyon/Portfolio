754cac48f39ce9989692f1fc2da331a0352bf447
ranyon <kwakumensahj@gmail.com>
2022-08-20 13:07:05 +0000

    tried using useref to pass dataa

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import { NavBar } from './components/Navbar';
 import Home from './components/pages/HomePage/Home';
 import Skills from './components/pages/SkillsPage/Skills';
 import Projects from './components/pages/ProjectPage/Projects'
+import { useRef ,useEffect} from 'react';
 
 function App() {
+     const projects = useRef(null)
+
+     useEffect(()=>{
+      console.log(projects.current,App.js)
+     }, [])
   return (
     <div className="">
-      <NavBar/>
-      <Home/>
-      <Skills/>
-      <Projects/>
+      <NavBar forwardedRef={projects}/>
+      <Home  />
+      <Skills />
+      <Projects forwardedRef={projects} />
       
       
     </div>
