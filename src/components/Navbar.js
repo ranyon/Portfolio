459b48af2b9587c55bc4afd95adfdae5d56db273
ranyon <kwakumensahj@gmail.com>
2022-08-21 11:36:09 +0000

    removed useRef

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,36 +2,39 @@ import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { useRef } from 'react';
+import './NavBar.css';
+import { useState } from 'react';
 
 
 
 
 
-export const NavBar = ({forwardedRef}) =>{
-  const home = useRef(null);
-  const skills = useRef(null);
-  const projects = useRef(null);
- 
-  const scrollToSection = (elementRef) =>{
-    window.scrollTo({
-      top:elementRef.current.offsetTop,
-      behavior:'smooth'
-    })
+export const NavBar = () =>{
+  const[navbar,setNavbar]= useState(false)
+
+  const changeBackground=()=>{
+    if(window.scrollY >= 80){
+      setNavbar(true)
+    }  else{
+      setNavbar(false);
+    }
+
   }
-    return(
-        <Navbar sticky="top" expand="lg">
+window.addEventListener('scroll',changeBackground)
+  return(
+    <Navbar sticky="top" expand="lg" className={navbar ? 'navbar-active '  :'navbar'}>
         <Container>
           <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className='ml-auto'>
+            <Nav className='ml-auto '>
               <Nav.Link href='#home' >Home</Nav.Link>
               <Nav.Link  href='#skills'>Skills</Nav.Link>
-              <Nav.Link  href='#projects'>Projectt</Nav.Link>
+              <Nav.Link  href='#projects'>Projects</Nav.Link>
             </Nav>
     
           </Navbar.Collapse>
         </Container>
       </Navbar>
     )
-}
+  }
