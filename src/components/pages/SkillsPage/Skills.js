5f585b5b2e31f4a751257ba8ed4f55594af14849
ranyon <kwakumensahj@gmail.com>
2022-08-21 17:08:41 +0000

    added col bootstrap

--- src/components/pages/SkillsPage/Skills.js
+++ src/components/pages/SkillsPage/Skills.js
@@ -1,28 +1,38 @@
 import React from 'react'
 import './Skills.css'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 const Skills =() => {
   return (
     <div id='skills'>
       <div className="SkillContainer" id='skils'>
-        <h1>Skills</h1>
-        <p className="skill-paragraph">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
-          inventore harum dignissimos error perspiciatis commodi facilis sed
-          architecto cumque quasi.
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
-          inventore harum dignissimos error perspiciatis commodi facilis sed
-          architecto cumque quasi.
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
-          inventore harum dignissimos error perspiciatis commodi facilis sed
-          architecto cumque quasi.
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
-          inventore harum dignissimos error perspiciatis commodi facilis sed
-          architecto cumque quasi.
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
-          inventore harum dignissimos error perspiciatis commodi facilis sed
-          architecto cumque quasi.
-        </p>
+      <Container>
+        <Row>
+          <Col>
+          
+          <h1>Skills</h1>
+          <p className="skill-paragraph">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+            inventore harum dignissimos error perspiciatis commodi facilis sed
+            architecto cumque quasi.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+            inventore harum dignissimos error perspiciatis commodi facilis sed
+            architecto cumque quasi.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+            inventore harum dignissimos error perspiciatis commodi facilis sed
+            architecto cumque quasi.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+            inventore harum dignissimos error perspiciatis commodi facilis sed
+            architecto cumque quasi.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+            inventore harum dignissimos error perspiciatis commodi facilis sed
+            architecto cumque quasi.
+          </p>
+          </Col>
+        </Row>
+      </Container>
       </div>
     </div>
   );
