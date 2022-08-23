dc77d9b41a449c0ab9da1363c621a6b0c0bad2ec
ranyon <kwakumensahj@gmail.com>
2022-08-23 15:59:17 +0000

    fixing map

--- src/components/pages/SkillsPage/Skills.js
+++ src/components/pages/SkillsPage/Skills.js
@@ -3,24 +3,53 @@ import './Skills.css'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
-const Skills =() => {
+const Skills =({skillLogo,skillName}) => {
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
+  
+
   return (
-    <div id='skills'>
-      <div className="SkillContainer" id='skils'>
-      <Container>
-        <Row>
-          <Col>
-          <div className='skillLogo'>
-          <div className='skillName'></div>
-          </div>
-          
-        
-          </Col>
-        </Row>
-      </Container>
-      </div>
-    </div>
+   <div>
+    <h1>skill Logo</h1>
+    <p>skill Name</p>
+   </div>
   );
 }
 
