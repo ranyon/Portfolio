8a2b48c95ffac86e770916bd3338f7e64974d01d
ranyon <kwakumensahj@gmail.com>
2022-08-25 12:00:16 +0000

    to store skill data

---
+++ src/components/pages/SkillsPage/skillData.js
@@ -0,0 +1,8 @@
+export const skillData =[
+    {skillLogo:'HTML',skillName:'html'},
+    {skillLogo:'CSS',skillName:'css'},
+    {skillLogo:'JAVASCRIPT',skillName:'js'},
+    {skillLogo:'REACT',skillName:'react'},
+    {skillLogo:'REACT-NATIVE',skillName:'react-native'}
+]
+
